1e5a0afce927b5c2f920443573c8d829d7c86f84
Björn Andersson <andersson.nc@gmail.com>
2013-01-22 22:31:59 +0100

    Namespacing and bugfixes

--- example/js/punch.js
+++ example/js/punch.js
@@ -17,14 +17,14 @@
         attr = "",
         level = 0,
         firstrun = true;
- })(
+
     if(typeof(Object.prototype.punch) === "undefined") {
         Object.prototype.punch = function() {
             if(firstrun) {
-                var top_item = this.querySelector('[data-tmpl=item]');
+                var top_item = this.querySelector('[data-punch=item]');
                 var item = top_item;
                 var clone = "";
-                var json_obj = arguments[0][item.getAttribute('data-tmpl-val')];
+                var json_obj = arguments[0][item.getAttribute('data-punch-val')];
                 firstrun = false;
             }
             else {
@@ -66,20 +66,20 @@
 
 
                                 // value -> attribute 
-                                if(item.querySelector('[data-tmpl-attr*=' + designator + property + ']')) {
-                                    var attr = item.querySelector('[data-tmpl-attr*=' + designator + property + ']').getAttribute('data-tmpl-attr').split(';')[0];
+                                if(item.querySelector('[data-punch-attr*=' + designator + property + ']')) {
+                                    var attr = item.querySelector('[data-punch-attr*=' + designator + property + ']').getAttribute('data-punch-attr').split(';')[0];
                                     var value = json_obj[i][key];
                                     console.log(key + " " + value);
-                                    item.querySelector('[data-tmpl-attr*=' + designator + property + ']').setAttribute(attr,value);
-                                    item.querySelector('[data-tmpl-attr*=' + designator + property + ']').removeAttribute('data-tmpl-attr');
+                                    item.querySelector('[data-punch-attr*=' + designator + property + ']').setAttribute(attr,value);
+                                    item.querySelector('[data-punch-attr*=' + designator + property + ']').removeAttribute('data-punch-attr');
                                 }
 
                                 // value -> tag
-                                if(item.querySelector('[data-tmpl-val=' + designator + property + ']')) {
+                                if(item.querySelector('[data-punch-val=' + designator + property + ']')) {
                                     if(level > 0) {
                                         val += json_obj[i][key] + ",";
                                     }
-                                    item.querySelector('[data-tmpl-val=' + designator + property + ']').innerHTML = (val==="") ? json_obj[i][key] : val.substring(0,val.length-1);
+                                    item.querySelector('[data-punch-val=' + designator + property + ']').innerHTML = (val==="") ? json_obj[i][key] : val.substring(0,val.length-1);
                                 }
                                 
                             }
@@ -93,8 +93,8 @@
                         property = (designator.length > 0 ? "_" : "") + key;
                         console.log(level + ". " + designator + property + ": " + json_obj[key]);
                 
-                        if(item.querySelector('[data-tmpl-val=' + designator + property + ']')) {
-                            item.querySelector('[data-tmpl-val=' + designator + property + ']').innerHTML = json_obj[key];
+                        if(item.querySelector('[data-punch-val=' + designator + property + ']')) {
+                            item.querySelector('[data-punch-val=' + designator + property + ']').innerHTML = json_obj[key];
                         }
 
                     }
@@ -106,4 +106,4 @@
 
         }
     }
-);
+})();
